10a6996c6c29ac55124ed63c0a9bd11bb8d36be3
Doha Ahmed <dohaahmed656@gmail.com>
2024-04-01 23:35:18 +0200

    DohaMera

--- Controllers/ReceiptController.js
+++ Controllers/ReceiptController.js
@@ -1,9 +1,22 @@
 let ReceiptModel = require('../Models/RecieptModel');
 let ReceiptValidation = require('../Utils/ReceiptValidation');
+const { ObjectId } = require('mongodb');
 
 let GetAllReceipts = async (req, res) => {
     let allReceipts = await ReceiptModel.find();
     res.status(200).json({ data: allReceipts });
+}
+
+let GetReceiptByID = async (req, res) => {
+    if (req.params.id.length != 24)
+        return res.status(400).json({ message: "Invalid ID=" + req.params.id })
+    let ID = new ObjectId(req.params.id);
+    let receipt = await ReceiptModel.findById(ID);
+    if (receipt) {
+        res.status(200).json({ data: receipt })
+    } else {
+        res.status(400).json({ message: "Not Found ID=" + ID })
+    }
 }
 
 let AddReceipt = (req, res) => {
@@ -13,20 +26,41 @@ let AddReceipt = (req, res) => {
         newReceipt.save();
         res.status(200).json({ data: newReceipt, message: "Added Successfully" });
     } else {
+        res.status(400).json({ message: ReceiptValidation.errors[0].message });
+    }
+}
+
+let UpdateReceipt = async (req, res) => {
+    if (req.params.id.length != 24)
+        return res.status(400).json({ message: "Invalid ID=" + req.params.id })
+    if (ReceiptValidation(req.body)) {
+        let ID = new ObjectId(req.params.id);
+        let UpdatedReceipt = await ReceiptModel.findByIdAndUpdate(ID, { "product": req.body.product, "totalPrice": req.body.totalPrice });
+        if (UpdatedReceipt) {
+            res.status(200).json({ message: "Updated successfully" })
+        } else {
+            res.status(400).json({ message: "Not Found ID=" + ID })
+        }
+    } else {
         res.json({ message: ReceiptValidation.errors[0].message });
     }
 }
 
-let UpdateReceipt = (req, res) => {
-
-}
-
-let DeleteReceipt = (req, res) => {
-
+let DeleteReceipt = async (req, res) => {
+    if (req.params.id.length != 24)
+        return res.status(400).json({ message: "Invalid ID=" + req.params.id })
+    let ID = new ObjectId(req.params.id);
+    let deletedReceipt = await ReceiptModel.findByIdAndDelete(ID)
+    if (deletedReceipt) {
+        res.status(200).json({ message: "Deleted successfully" })
+    } else {
+        res.status(400).json({ message: "Not Found ID=" + ID })
+    }
 }
 
 module.exports = {
     GetAllReceipts,
+    GetReceiptByID,
     AddReceipt,
     UpdateReceipt,
     DeleteReceipt,
