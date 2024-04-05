f79d573cdec0a3a08a5a3551cd52e5fb3687ba9e
Doha Ahmed <dohaahmed656@gmail.com>
2024-04-05 07:18:24 +0200

    MeraDoha

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,5 +1,6 @@
 const UserModel = require('../Models/UserModel')
 const UserValidation = require('../Utils/UserValidation')
+const bcrypt = require('bcrypt')
 let GetUserByEmail = async (req, res) => {
     let user = await UserModel.findOne({ email: req.params.email.toLowerCase() });
     if (user) {
@@ -11,6 +12,9 @@ let GetUserByEmail = async (req, res) => {
 
 let UpdateUser = async (req, res) => {
     if (UserValidation(req.body)) {
+        let salt = await bcrypt.genSalt(10);
+        let hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
         let newUser = await UserModel.findOneAndUpdate({ email: req.params.email }, {
             "name": req.body.name, "email": req.body.email,
             "phone": req.body.phone, "gender": req.body.gender, "address": req.body.addres, "age": req.body.age, "password": req.body.password
